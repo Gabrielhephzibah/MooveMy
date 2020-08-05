2f47b707a0031e91e3304f52b93f4c2c78c0f0ec
Gabriel Hephzibah <gabrielhephzibah24@gmail.com>
2020-08-05 13:14:49 +0100

    apputils

--- app/src/main/java/com/enyata/android/mvvm_java/data/model/api/myData/VehicleCollection.java
+++ app/src/main/java/com/enyata/android/mvvm_java/data/model/api/myData/VehicleCollection.java
@@ -21,7 +21,7 @@ public class VehicleCollection {
 
     @SerializedName("component")
     @Expose
-    private String part ;
+    private String part;
 
     @SerializedName("section")
     @Expose
@@ -83,26 +83,25 @@ public class VehicleCollection {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VehicleCollection request = (VehicleCollection) o;
+        return Objects.equals(remark, request.remark) &&
+                Objects.equals(imageUrl, request.imageUrl) &&
+                Objects.equals(section, request.section) &&
+                Objects.equals(part, request.part);
 
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            VehicleCollection request = (VehicleCollection) o;
-            return Objects.equals(remark, request.remark) &&
-                    Objects.equals(imageUrl, request.imageUrl) &&
-                    Objects.equals(section, request.section) &&
-                    Objects.equals(part, request.part);
-
-        }
+    }
 
 
-        @Override
-        public int hashCode() {
-            int result = Objects.hash(remark, section,part);
-            result = 31 * result + Objects.hashCode(imageUrl);
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(remark, section, part);
+        result = 31 * result + Objects.hashCode(imageUrl);
 
-            return result;
-        }
+        return result;
+    }
 
     @Override
     public String toString() {
@@ -114,173 +113,7 @@ public class VehicleCollection {
                 '}';
     }
 
-    //        @Override
-//        public String toString() {
-//            return "Request{" +
-//                    "part='" + part + '\'' +
-//                    ", imageUrl=" + imageUrl +
-//                    ", remark='" + remark + '\'' +
-//
-//                    '}';
-//        }
-
 }
-
-//    private VehicleCollection() {
-//        //don not instantiate
-//    }
-//
-//    public static class Request {
-//
-//        @Expose
-//        @SerializedName("part")
-//        private String part;
-//
-//        @Expose
-//        @SerializedName("image_url")
-//        private List<String> imageUrl;
-//
-//        @Expose
-//        @SerializedName("remark")
-//        private String remark;
-//
-//        public Request(String part, List<String> imageUrl, String remark) {
-//            this.part = part;
-//            this.imageUrl = imageUrl;
-//            this.remark = remark;
-//        }
-//
-//        public String getPart() {
-//            return part;
-//        }
-//
-//        public void setPart(String part) {
-//            this.part = part;
-//        }
-//
-//        public List<String> getImageUrl() {
-//            return imageUrl;
-//        }
-//
-//        public void setImageUrl(List<String> imageUrl) {
-//            this.imageUrl = imageUrl;
-//        }
-//
-//        public String getRemark() {
-//            return remark;
-//        }
-//
-//        //        public String getPart() {
-////            return part;
-////        }
-////
-////        public List<String> getImageUrl() {
-////            return imageUrl;
-////        }
-////
-////        public String getRemark() {
-////            return remark;
-////        }
-////
-////        public void setPart(String part) {
-////            this.part = part;
-////        }
-////
-////        public void setImageUrl  imageUrl) {
-////            this.imageUrl = imageUrl;
-////        }
-//
-//        public void setRemark(String remark) {
-//            this.remark = remark;
-//        }
-//
-//        @Override
-//
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            VehicleCollection request = (VehicleCollection) o;
-//            return Objects.equals(remark, request.remark) &&
-//                    Objects.equals(imageUrl, request.imageUrl) &&
-//                    Objects.equals(part, request.part);
-//        }
-//
-//
-//        @Override
-//        public int hashCode() {
-//            int result = Objects.hash(remark, part);
-//            result = 31 * result + Objects.hashCode(imageUrl);
-//            return result;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "Request{" +
-//                    "part='" + part + '\'' +
-//                    ", imageUrl=" + imageUrl +
-//                    ", remark='" + remark + '\'' +
-//                    '}';
-//        }
-//    }
-
-
-//        @Override
-//        public boolean equals(Object object) {
-//            if (this == object) return true;
-//            if (object == null || getClass() != object.getClass()) return false;
-//            Request request = (Request) object;
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//                return Objects.equals(getPart(), request.getPart()) &&
-//                        Arrays.equals(getImageUrl(), request.getImageUrl()) &&
-//                        Objects.equals(getRemark(), request.getRemark());
-//            }
-//            else {
-//                return false;
-//            }
-//        }
-//
-//
-//        @Override
-//        public int hashCode() {
-//            int result = 0;
-//            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-//                result = Objects.hash(getPart(), getRemark());
-//            }
-//            result = 31 * result + Arrays.hashCode(getImageUrl());
-//            return result;
-//        }
-
-
-    //        @Override
-//        public boolean equals(Object obj) {
-//            if (this == obj) {
-//                return true;
-//            }
-//
-//            if (obj == null || getClass() != obj.getClass()) {
-//                return false;
-//            }
-//
-//          VehicleCollection request = (Request)obj;
-//
-//            if (part != null ? !part.equals(request.part) : request.part != null) {
-//                return false;
-//            }if (imageUrl != null ? !imageUrl.equals(request.imageUrl) : request.imageUrl != null) {
-//                return false;
-//            }
-//            return remark != null ? !remark.equals(request.remark) : request.remark != null;
-//
-//        }
-
-
-//        @Override
-//        public int hashCode() {
-//            int result = 0;
-//            result = 31 * result + (part != null ? part.hashCode() : 0);
-//            result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
-//            result = 31 * result + (remark != null ? remark.hashCode() : 0);
-//            return result;
-//
 
 
 
